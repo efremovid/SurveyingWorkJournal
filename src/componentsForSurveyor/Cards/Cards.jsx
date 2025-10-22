3c43cdfb7ecf7f28f304a584421d8cd3a3340d35
efremovid <i.efremov.psk@yandex.ru>
2025-10-22 21:51:21 +0300

    осталось чутка

--- src/componentsForSurveyor/Cards/Cards.jsx
+++ src/componentsForSurveyor/Cards/Cards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import styles from "./Cards.module.scss";
 
@@ -9,9 +9,42 @@ const Cards = ({
   handleChangeComment,
   comment,
 }) => {
+  const [selectedStatus, setSelectedStatus] = useState("Все заявки");
+
+  const filterByStatus = (arr) => {
+    if (selectedStatus === "Все заявки") {
+      return arr;
+    } else {
+      return arr.filter(
+        (el) => el.status.toLowerCase() === selectedStatus.toLowerCase()
+      );
+    }
+  };
+
+  const statuses = [
+    "Все заявки",
+    "Отправлено инженеру-геодезисту",
+    "Принято в работу",
+    "Выполнено",
+    "Отклонено",
+  ];
+
   return (
     <div className={styles.container}>
-      {applications.map((application) => (
+      <div className={styles.buttons}>
+        {statuses.map((status) => (
+          <button
+            key={status}
+            className={`${styles.button} ${
+              selectedStatus === status ? styles.active : ""
+            }`}
+            onClick={() => setSelectedStatus(status)}
+          >
+            {status}
+          </button>
+        ))}
+      </div>
+      {filterByStatus(applications).map((application) => (
         <Card
           application={application}
           changeStatus={changeStatus}
